c95811bc792224fabac5d85b4716b3baec04919a
ivickypr05 <iko.pratama.ti.20@cic.ac.id>
2023-02-24 08:04:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>VCiky</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Iko Vicky Pratama</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/vicky.jpg" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#organization">Organization</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#achievement">Achievement</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Iko
+                        <span class="text-primary">Vicky</span>
+                        Pratama
+                    </h1>
+                    <div class="subheading mb-4">
+                        place of birth • Cirebon, 5 September 2022 • I'm 20 Years Old ^_^
+                    </div>
+                    <p class="lead mb-5">From a young age I have loved everything about Technology Science, until the Study Program I took at 
+                        Vocational high School was Computer Network Engineering and continued until now in college I took the 
+                        Informatics Engineering study program</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://github.com/ivickypr05"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="https://instagram.com/ivickypr_?igshid=NmQ2ZmYxZjA="><i class="fab fa-instagram"></i></a>
+                        <a class="social-icon" href="https://wa.me/089519343409"><i class="fab fa-whatsapp"></i></a>
+                        <a class="social-icon" href="https://www.facebook.com/vicky.nak.scout?mibextid=ZbWKwL"><i class="fab fa-facebook-f"></i></a>
+                        <a class="social-icon" href="https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=ikovickyp@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Internship Students</h3>
+                            <div class="subheading mb3">at PT Bitsnet (Bina Informatika Solusi) Cirebon, Indonesia</div>
+                            <p> BitsNet (PT. Bina Informatika Solusindo) was first established in 2014 as a complete Internet Service Provider. 
+                                BitsNet offers full scale services as a leading Internet Service Provider (ISP) and a leading Managed Service Provider (MSP). 
+                                in it I help monitor problems with client network infrastructure and fix network infrastructure problems, Install and Configure CCTV NOC, and Technicians help install the Internet using the Wireless (point to multi point) method. </p>
+                        </div>
+                        
+                 <div class="flex-shrink-0"><span class="text-primary">December 2018 - February 2019 </span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Independent Study Kampus Merdeka</h3>
+                            <div class="subheading mb-3">Full Stack Web Developer with Gits.id Company</div>
+                            <p>Here I learned about the fundamentals for a career as a Full Stack Web Developer individually at the beginning of class, then continued with the Capstone Project program (e-commerce base project) which was carried out in groups, 
+                                besides that at the end of the program session participants were taught to manage web applications by utilizing technology cloud (Google Cloud Platform), it is hoped that participants will be able to create web-based applications that can be useful for MSMEs so that they continue to grow..</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">February 2023 - Present</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Teacher</h3>
+                            <div class="subheading mb-3">Al-Qur'an Education Nurul Falah Semeru Asih Cirebon, Indonesia</div>
+                            <p>It is a place to study Islam, here Im Teaching Islamic Religious Education, Reading Al-Qur'an and Tahfidz, and educating Santri to be civilized and have good ethics.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">March 2018 - Present</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Catur Insan Cendekia University</h3>
+                            <div class="subheading mb-3">Informatics Engineering</div>
+                            <p>GPA: 3.98</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">September 2020 - September 2024(expected)</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Vocational High School Informatika Al-Irsyad Al-Islamiyyah</h3>
+                            <div class="subheading mb-3">High School Diploma in Computer and Network Engineering</div>
+                           
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">July 2002 - June 2020</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Hardskill</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-java"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-python"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-laravel"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-figma"></i></li>
+                        
+                    </ul>
+                    <div class="subheading mb-3">Softskill</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Communications
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Team Work
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Public Spearking
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Critical Thinking
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Leadership
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Problem Solving
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Organization -->
+            <section class="resume-section" id="organization">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Organization</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Informatics Engineering Student Association (Himatif)</h3>
+                            <div class="subheading mb-2">At the Catur Insan Cendekia University</div>
+                            <div>Resource and Development coordinator</div>
+                            <button class="btn btn-light border-0 shadow text-primary main-color btn-certificate"
+                            onclick="Certificate('../assets/img/certificate/radcertif.jpg', 'Resource and Development Coordinator', 'HIMATIF')">Certificate/Photo</button>
+
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Januari 2022 - Januari 2023</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">E-sport MLBB</h3>
+                            <div class="subheading mb-2">At the Catur Insan Cendekia University</div>
+                            <div>Captain Team Rise (UCIC) games categori Mobile Legends Bang-bang</div>
+                            <button class="btn btn-light border-0 shadow text-primary main-color btn-certificate"
+                            onclick="showCertificate('.. assets/img/certificate/radcertif.jpg', 'Junior Graphic Designer', 'BNSP Indonesia')">Certificate/Photo</button>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary"> November 2021 - until now</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Basket Ball</h3>
+                            <div class="subheading mb-2">At the Catur Insan Cendekia University</div>
+                            <div>Center position in the basketball team UCIC</div>
+                            <button class="btn btn-light border-0 shadow text-primary main-color btn-certificate"
+                            onclick="showCertificate('assets/img/certificate/radcertif.jpg', 'Junior Graphic Designer', 'BNSP Indonesia')">Certificate/Photo</button>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary"> September 2022 - until now</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Achievement-->
+            <section class="resume-section" id="achievement">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Achievement & Certifications</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Contributor in International Quotes Writing Competition - Eduvement.id
+                        </li>
+                        <button class="btn btn-light border-0 shadow text-primary main-color btn-certificate mb-3"
+                            onclick="Certificate('../assets/img/certificate/radcertif.jpg', 'Resource and Development Coordinator', 'HIMATIF')">Certificate</button>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Creative Creator in International Quotes Writing Competition - Eduvement.id
+                        </li>
+                        <button class="btn btn-light border-0 shadow text-primary main-color btn-certificate mb-3"
+                            onclick="Certificate('../assets/img/certificate/radcertif.jpg', 'Resource and Development Coordinator', 'HIMATIF')">Certificate</button>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Highest GPA in Informatics Engineering major - Catur Insan Cendekia University 2022
+                        </li>
+                        <button class="btn btn-light border-0 shadow text-primary main-color btn-certificate mb-3"
+                            onclick="Certificate('../assets/img/certificate/radcertif.jpg', 'Resource and Development Coordinator', 'HIMATIF')">Certificate</button>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - MLBB Competition - Semarak UCIC 2023
+                        </li>
+                        <button class="btn btn-light border-0 shadow text-primary main-color btn-certificate mb-3"
+                            onclick="Certificate('../assets/img/certificate/radcertif.jpg', 'Resource and Development Coordinator', 'HIMATIF')">Certificate</button>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - MLBB Competition - SupperChallenge Dare to Win 2021
+                        </li>
+                        <button class="btn btn-light border-0 shadow text-primary main-color btn-certificate mb-3"
+                            onclick="Certificate('../assets/img/certificate/radcertif.jpg', 'Resource and Development Coordinator', 'HIMATIF')">Certificate</button>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            3
+                            <sup>th</sup>
+                            Place - Pencak Silat Class E - Pop Kota 2017, Cirebon
+                        </li>
+                        <button class="btn btn-light border-0 shadow text-primary main-color btn-certificate mb-3"
+                            onclick="Certificate('../assets/img/certificate/radcertif.jpg', 'Resource and Development Coordinator', 'HIMATIF')">Certificate</button>
+                    </ul>
+                </div>
+            </section>
+        </div>
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ js/custom.js
@@ -0,0 +1,19 @@
+// Certificate
+
+function Certificate(imgSrc, field, from) {
+  Swal.fire({
+    title: field,
+    text: "$from",
+    imageUrl: imgSrc,
+    imageWidth: "70%",
+    imageAlt: "Certificate-Img",
+  });
+}
+function test(title, desc) {
+  const element = document.getElementById(desc);
+  Swal.fire({
+    title: `${title}`,
+    html: element.innerHTML,
+    confirmButtonText: "Ok",
+  });
+}
